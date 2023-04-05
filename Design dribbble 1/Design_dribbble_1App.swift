21e1e19d52197c017529bfbb18045d9a27afb63a
Eduardo Herrera <edauardodesarrollador@icloud.com>
2023-04-05 17:01:31 +0200

    Initial Commit

---
+++ Design dribbble 1/Design_dribbble_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Design_dribbble_1App.swift
+//  Design dribbble 1
+//
+//  Created by Eduardo Herrera on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_dribbble_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
